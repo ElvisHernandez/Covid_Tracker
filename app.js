ab44fad6ddf31efb4f477fcfe2fbcb7c97e5b07d
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-05 04:09:16 -0400

    added get all covid data route

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 
+app.get('/api', async (req,res,next) => {
+
+    const { data } = await axios('https://covidtracking.com/api/v1/states/daily.json')
+
+    res.send(data)
+})
 
 
 app.listen(PORT, () => {
